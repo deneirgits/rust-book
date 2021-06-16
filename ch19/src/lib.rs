01913c7086f1a7653741dfe12f7166d42f1159f7
Deneir Uy <deneiruy@gmail.com>
2021-06-16 03:46:38 +0000

    Partial ch19

---
+++ ch19/src/lib.rs
@@ -0,0 +1,12 @@
+use std::ops::Add;
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, rhs: Meters) -> Self::Output {
+        Millimeters(self.0 + (rhs.0 * 1000))
+    }
+}
---
+++ ch19/src/main.rs
@@ -0,0 +1,14 @@
+use std::fmt::{self, Display};
+
+struct Wrapper(Vec<String>);
+
+impl Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+
+fn main() {
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
+}
